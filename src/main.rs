529b5c18a6c757dab5b57e7a89e6f735eb23a673
Fisher Sun <f.sun@wustl.edu>
2023-04-25 02:45:55 -0500

    Allow passing multiple args

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,18 @@ fn main() {
     let packages = nix_editor::read::getarrvals(&content, "home.packages").unwrap();
 
     let args: Vec<String> = env::args().collect();
-    let item = &args[1];
 
-    if packages.contains(item) {
-        println!("home.nix already contains this package. stopping.");
-        return;
+    let mut to_add= Vec::new();
+    for arg in &args[1..] {
+        if packages.contains(&arg) {
+            println!("home.nix already contains {}, skipping it", arg);
+        } else {
+            to_add.push(arg);
+        }
     }
 
-    println!("adding {} to home.nix", item);
-    let result = nix_editor::write::addtoarr(&content, "home.packages", vec![item].into_iter().cloned().collect());
+    println!("adding {:?} to home.nix", to_add);
+
+    let result = nix_editor::write::addtoarr(&content, "home.packages", to_add.into_iter().cloned().collect());
     println!("{}", result.unwrap());
 }
